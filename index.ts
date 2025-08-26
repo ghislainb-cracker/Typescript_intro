7c97a4f5eb874861d4c4806e9f807f1ed68c510e
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-26 22:31:06 +0200

    fifth commit

--- index.ts
+++ index.ts
@@ -1,8 +1,14 @@
 // const data: string = "my name is ghislain"
 // console.log(data)
 
-function add(num1: number, num2: number): number{
-    return num1 + num2
+// function add(num1: number, num2: number): number{
+//     return num1 + num2
+// }
+
+// console.log(add(2,5));
+
+function formatId(want: number | string): string{
+    return want.toString();
 }
 
-console.log(add(2,5));
+console.log(formatId(2));
