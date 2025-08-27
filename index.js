8a05939cd69527bf5c06388ceb49b3825db375ff
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-27 22:09:30 +0200

    19 commit

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 // const data: string = "my name is ghislain"
 // console.log(data)
 function printUser(user) {
-    return user;
+    console.log(user);
 }
-printUser({ name: "ghislain", email: "byimbog250@gmail.com" });
+printUser({}); // Empty object
+printUser({ name: "ghislain" }); // Only name
+printUser({ email: "byimbog250@gmail.com" }); // Only email
+printUser({ name: "ghislain", email: "byimbog250@gmail.com" }); // Name and email
+printUser({ id: 1, name: "ghislain", email: "byimbog250@gmail.com" }); // All properties
