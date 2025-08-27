e73afd0f3bc61ef72f7b699e7ed71ed5b4116368
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-27 22:10:22 +0200

    21 commit

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 function printUser(user) {
     console.log(user);
 }
-printUser({}); // Empty object
-printUser({ name: "ghislain" }); // Only name
-printUser({ email: "byimbog250@gmail.com" }); // Only email
-printUser({ name: "ghislain", email: "byimbog250@gmail.com" }); // Name and email
-printUser({ id: 1, name: "ghislain", email: "byimbog250@gmail.com" }); // All properties
+printUser({});
+printUser({ name: "ghislain" });
+printUser({ email: "byimbog250@gmail.com" });
+printUser({ name: "ghislain", email: "byimbog250@gmail.com" });
+printUser({ id: 1, name: "ghislain", email: "byimbog250@gmail.com" });
