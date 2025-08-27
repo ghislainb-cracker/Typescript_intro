24a49accdf17f564e46fb4b6c61e41c8bc3d805e
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-27 22:09:14 +0200

    18 commit

--- index.ts
+++ index.ts
@@ -47,7 +47,11 @@ type User = {
 type OptionalUser = Partial<User>
 
 function printUser(user: OptionalUser){
-    return user
+    console.log(user)
 }
 
-printUser({name: "ghislain", email:"byimbog250@gmail.com"})
+printUser({}); // Empty object
+printUser({name: "ghislain"}); // Only name
+printUser({email: "byimbog250@gmail.com"}); // Only email
+printUser({name: "ghislain", email: "byimbog250@gmail.com"}); // Name and email
+printUser({id: 1, name: "ghislain", email: "byimbog250@gmail.com"}); // All properties
