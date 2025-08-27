6b473151b4deae7c96b5b51b8fadfbc60c78c206
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-27 21:49:21 +0200

    15 commit

--- index.js
+++ index.js
@@ -1,11 +1,28 @@
 // const data: string = "my name is ghislain"
 // console.log(data)
-function printUser(user) {
-    console.log("User ".concat(user.id, ": ").concat(user.name, " (").concat(user.email, ")"));
+// function add(num1: number, num2: number): number{
+//     return num1 + num2
+// }
+// console.log(add(2,5));
+// function formatId(want: number | string): string{
+//     return want.toString();
+// }
+// console.log(formatId(2));
+// interface User {
+//     id: number,
+//     name: string,
+//     email: string
+// }
+// function printUser(user: User){
+//     console.log(`User ${user.id}: ${user.name} (${user.email})`);
+// }
+// const user1: User = {
+//     id: 2025,
+//     name: "Ghislain Byimbo",
+//     email: "byimbog250@gmail.com"
+// }
+// printUser(user1);
+function wrapped(value) {
+    return [value];
 }
-var user1 = {
-    id: 2025,
-    name: "Ghislain Byimbo",
-    email: "byimbog250@gmail.com"
-};
-printUser(user1);
+console.log(wrapped("one"));
