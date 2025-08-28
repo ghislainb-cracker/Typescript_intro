fd2f212fca16e4efdb72d7a2d36a2091a6f57b1d
Ghislain Byimbo <byimbog250@gmail.com>
2025-08-28 21:40:45 +0200

    23 commit

--- index.js
+++ index.js
@@ -8,3 +8,16 @@ printUser({ name: "ghislain" });
 printUser({ email: "byimbog250@gmail.com" });
 printUser({ name: "ghislain", email: "byimbog250@gmail.com" });
 printUser({ id: 1, name: "ghislain", email: "byimbog250@gmail.com" });
+function printLength(input) {
+    if (typeof input === 'string') {
+        console.log("string length: ".concat(input.length));
+    }
+    else if (Array.isArray(input)) {
+        console.log("Array length: ".concat(input.length));
+    }
+    else {
+        console.log("Invalid datatype");
+    }
+}
+printLength("ghislain");
+printLength([1, 2, 3, 4, 5]);
